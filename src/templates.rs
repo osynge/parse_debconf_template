ccd3c35d0a9495489705d761b56c40b87cd08d23
Owen Synge <osynge@googlemail.com>
2020-06-18 14:57:29 +0200

    unused variable

--- src/templates.rs
+++ src/templates.rs
@@ -201,7 +201,7 @@ mod tests {
                 );
                 assert!(false);
             }
-            Err(Err::Incomplete(e)) => {}
+            Err(Err::Incomplete(_e)) => {}
         }
     }
 
